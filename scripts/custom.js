b708209958cb73473530425f87209604054c80f3
borgerry <borodingerry@gmail.com>
2021-06-06 19:14:36 +0300

    End of localization

---
+++ scripts/custom.js
@@ -0,0 +1,47 @@
+window.avastGlobals = window.avastGlobals || {};
+window.avastGlobals.web = {
+  domain: "localhost",
+  pathFromRoot: "en-us/index",
+  fileName: "index",
+  locale: "en-us",
+  ulocale: "/en-us",
+  RootPath: "../",
+  language: "en",
+  numberDecimalSeparator: ".",
+  numberThousandSeparator: ",",
+  responsive: true,
+  contentGroup: 'Consumer',
+  pageName: 'en-us | en-us/index',
+  'notification-overlay-for-wrong-download': {
+    'this-is-a-temp-index': {
+      '					alternative-links': {
+        android: '/download-thank-you.php?product=AMS',
+        mac: '/download-thank-you.php?product=MAC-FREE-ONLINE',
+        ios: '/download-thank-you.php?product=IMS'
+      },
+      'alternative-tracking': {
+        android: 'AMS',
+        mac: 'MAC-FREE-ONLINE',
+        ios: 'IMS'
+      },
+      'button-selector': '[href*=\"download-thank-you.php?product=FAV-ONLINE\"]',
+      'product-id': 'FAV-ONLINE',
+      'supported-platform': 'windows'
+    },
+    'free-antivirus-download-v2': {
+      'alternative-links': {
+        android: '/download-thank-you.php?product=AMS',
+        mac: '/download-thank-you.php?product=MAC-FREE-ONLINE',
+        ios: '/download-thank-you.php?product=IMS'
+      },
+      'alternative-tracking': {
+        android: 'AMS',
+        mac: 'MAC-FREE-ONLINE',
+        ios: 'IMS'
+      },
+      'button-selector': '[href*=\"download-thank-you.php?product=FAV-ONLINE\"]',
+      'product-id': 'FAV-ONLINE',
+      'supported-platform': 'windows'
+    }
+  }
+};
